509d35bb7da0c502a43ff159e80682bea73fc50e
Pete Johns <pete.johns@envato.com>
2019-06-24 13:31:50 +1000

    Handle profile names with Unicode characters

--- ffp.rb
+++ ffp.rb
@@ -1,26 +1,23 @@
-theQuery = ARGV[0]
+# frozen_string_literal: true
 
-# get profiles
-ffpdir = "~/Library/Application\\ Support/Firefox/Profiles/"
-profiles = `ls #{ffpdir}`.split("\n")
-# STDERR.puts profiles
+FFPDIR = File.join(Dir.home, 'library', 'application support', 'firefox', 'profiles')
+PROFILES = Dir.glob(File.join(FFPDIR, '*')).sort
+# warn(PROFILES)
 
 # Start the XML string that will be sent to Alfred. This just uses strings to avoid dependencies.
-xmlString = "<?xml version=\"1.0\"?>\n<items>\n"
-profiles.each do | profile |
-    # STDERR.puts profile
-    profileClean = `echo \"#{profile}\" | grep -oEi "\\.([A-z0-9\-])+$"`.delete('.').delete("\n")
+XML_HEAD = %(<?xml version="1.0"?>\n<items>)
+XML_PROFILES = PROFILES.map do |profile|
+  name = File.extname(profile).delete('.')
 
-    # Assemble this item's XML string for Alfred. See http://www.alfredforum.com/topic/5-generating-feedback-in-workflows/
-    thisXmlString = "\t<item uid=\"#{profile}\" arg=\"#{profileClean}\">
-        <title>#{profileClean}</title>
-        <subtitle>#{ffpdir}#{profile}</subtitle>
-    </item>\n"
-
-    # Append this process's XML string to the global XML string.
-    xmlString += thisXmlString
-end
+  # Assemble this item's XML string for Alfred. See http://www.alfredforum.com/topic/5-generating-feedback-in-workflows/
+  %(
+    <item uid="#{profile}" arg="#{name}">
+      <title>#{name}</title>
+      <subtitle>#{FFPDIR}#{profile}</subtitle>
+    </item>)
+end.join
 
 # Finish off and echo the XML string to Alfred.
-xmlString += "</items>"
-puts xmlString
+XML_TAIL = '</items>'
+
+puts [XML_HEAD, XML_PROFILES, XML_TAIL].join
