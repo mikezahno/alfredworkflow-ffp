b1fb7737183cbb7926dced81529037747621e82d
Pete Johns <paj+github@johnsy.com>
2020-12-16 17:55:47 +1100

    Order by most recently updated

--- ffp.rb
+++ ffp.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 FFPDIR = File.join(Dir.home, 'Library', 'Application Support', 'Firefox', 'Profiles')
-PROFILES = Dir.glob(File.join(FFPDIR, '*')).sort
+PROFILES = Dir.glob(File.join(FFPDIR, '*')).sort_by { |f| File.mtime(f) }
 warn(PROFILES) if ENV['DEBUG']
 
 # Start the XML string that will be sent to Alfred. This just uses strings to avoid dependencies.
 XML_HEAD = %(<?xml version="1.0"?>\n<items>)
-XML_PROFILES = PROFILES.map do |profile|
+XML_PROFILES = PROFILES.reverse_each.map do |profile|
   name = File.basename(profile).split('.', 2).last
   compatibility_filename = File.join(profile, 'compatibility.ini')
   icon_dir = File.read(compatibility_filename)
